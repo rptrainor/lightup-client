3a90c2b48ce601c083902ad1d55564053f0df670
Ryan Trainor <rptrainor@gmail.com>
2023-12-10 23:22:48 -0300

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [solidJs(), sitemap()]
+  integrations: [solidJs(), sitemap(), partytown()]
 });
