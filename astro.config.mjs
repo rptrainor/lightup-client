be6da54d2714e476b465309386272c54bb86efd4
Ryan Trainor <rptrainor@gmail.com>
2023-12-10 23:26:00 -0300

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [solidJs(), sitemap(), partytown()]
+  integrations: [solidJs(), sitemap(), partytown(), tailwind()]
 });
