0b3f282818d621fbed92a652ce779fe4e589c34d
Ryan Trainor <rptrainor@gmail.com>
2023-12-10 23:16:38 -0300

    add sitemap for better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
-  integrations: [solidJs()]
+  integrations: [solidJs(), sitemap()]
 });
