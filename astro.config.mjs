545d3751805b80dcc7dc1add829a911777452c9d
Ryan Trainor <rptrainor@gmail.com>
2023-12-10 23:14:09 -0300

    add solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [solidJs()]
 });
