2c754c6980c396e094e78992c52ed0f013b2f3c2
Ryan Trainor <rptrainor@gmail.com>
2023-12-11 17:11:26 -0300

    remove tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,27 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lightup-client.pages.dev/',
+  site: "https://lightup-client.pages.dev/",
   output: "server",
   image: {
-    service: passthroughImageService()
+    service: passthroughImageService(),
   },
   adapter: cloudflare({
-    imageService: "passthrough"
+    imageService: "passthrough",
   }),
   integrations: [
     solidJs(),
-    tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false
-  }),
-  sitemap(),
-  partytown({
-    config: {
-      forward: ['dataLayer.push', "posthog","intercomSettings"]
-    },
-  })]
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "posthog", "intercomSettings"],
+      },
+    }),
+  ],
 });
