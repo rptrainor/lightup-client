fb652d9196ded91b7173d7f0042a97faed257e8b
Ryan Trainor <rptrainor@gmail.com>
2023-12-10 23:32:59 -0300

    modify the astro config to optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,32 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lightup-client.pages.dev/',
   output: "server",
-  adapter: cloudflare(),
-  integrations: [solidJs(), sitemap(), partytown(), tailwind()]
+  image: {
+    service: passthroughImageService()
+  },
+  adapter: cloudflare({
+    imageService: "passthrough"
+  }),
+  integrations: [
+    solidJs(),
+    tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false
+  }),
+  sitemap(),
+  partytown({
+    config: {
+      forward: ['dataLayer.push', "posthog"]
+    },
+  })]
 });
