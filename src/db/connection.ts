383f3f63115853b111643946cef86bd2d995c032
Ryan Trainor <rptrainor@gmail.com>
2023-12-14 13:31:53 -0300

    add db connection and astro view transitions

---
+++ src/db/connection.ts
@@ -0,0 +1,18 @@
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
+
+// Constants for environment variables
+const PUBLIC_SUPABASE_URL = import.meta.env.PUBLIC_SUPABASE_URL;
+const PUBLIC_SUPABASE_ANON_KEY = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+
+// Validate environment variables
+if (!PUBLIC_SUPABASE_URL || !PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing Supabase environment variables. Make sure PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_ANON_KEY are set.');
+}
+
+// Initialize Supabase client
+export const supabase: SupabaseClient = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false
+  }
+});
