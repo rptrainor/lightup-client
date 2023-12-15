bbe1a31e8042b6f664ef0672a2e7a7777f8e1dd5
Ryan Trainor <rptrainor@gmail.com>
2023-12-15 05:41:43 -0300

    fix the bug about not being able to dismiss auth modal

--- src/components/AuthModel.tsx
+++ src/components/AuthModel.tsx
@@ -33,7 +33,7 @@ const AuthModel = () => {
   });
 
   return (
-    <Show when={userState().status === "loggedOut"} fallback={null}>
+    <Show when={userState().status === "loggedOut" && !dismissed()} fallback={null}>
       <div
         class='w-64 sm:w-72 border-4 border-solid border-brand_black fixed right-1 bottom-2 sm:bottom-4 sm:right-2 bg-brand_white pt-16 sm:pt-20 pb-2 px-2 sm:px-4 flex flex-col items-center gap-2 sm:gap-4 z-20'
       >
