318190c73904b20232b2b4dc513e383242ee6e6d
Ryan Trainor <rptrainor@gmail.com>
2023-12-17 12:56:07 -0300

    syncing the user with public user table from client

--- src/stores/auth_store.ts
+++ src/stores/auth_store.ts
@@ -10,12 +10,29 @@ type UserState =
 
 const [userState, setUserState] = createSignal<UserState>({ status: 'initial', user: null });
 
+export async function addUserToPublicTable(user: User) {
+  try {
+    const { data, error } = await supabase
+      .from('users')
+      .upsert([{ ...user }]); // Using upsert to insert or update based on the 'id'
+
+    if (error) {
+      console.error("Error in addUserToPublicTable:", error);
+      throw new Error("Error adding/updating user");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error in addUserToPublicTable:", error);
+    throw new Error("Error in addUserToPublicTable");
+  }
+}
 
 const handleCheckUser = async () => {
   const { data: { user: supabaseUser } } = await supabase.auth.getUser();
 
   if (supabaseUser && Object.keys(supabaseUser).length > 0) {
-    // Assuming supabaseUser has the necessary properties of your User type
+    // Transform the user data
     const transformedUser: User = {
       id: supabaseUser.id,
       created_at: supabaseUser.created_at,
@@ -28,6 +45,9 @@ const handleCheckUser = async () => {
       picture: supabaseUser.user_metadata.avatar_url,
     };
     setUserState({ status: 'loggedIn', user: transformedUser });
+
+    // Sync user with public.users table
+    await addUserToPublicTable(transformedUser);
   } else if (supabaseUser === null) {
     setUserState({ status: 'loggedOut', user: null });
   } else {
