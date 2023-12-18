6f7bb97180c8c762844b4e3f5576d0b46a8db889
Ryan Trainor <rptrainor@gmail.com>
2023-12-18 17:55:38 -0300

    Started working on grow page

---
+++ src/components/ThankYouLetter.tsx
@@ -0,0 +1,37 @@
+import { Show, createEffect } from "solid-js";
+import { userState } from "~/stores/auth_store";
+import type { Project } from "~/components/ProjectLikeButton";
+
+type Props = {
+  project: Project
+}
+
+const ThankYouLetter = (props: Props) => {
+  createEffect(() => {
+    console.log('ThankYouLetter', { props, user: userState().user });
+  });
+  return (
+    <div
+      class='flex flex-col gap-4 bg-brand_white p-4 border-solid border-4 border-brand_black text-brand_black'
+    >
+      <Show when={userState().user?.full_name || userState().user?.name} fallback={<p>Hi,</p>}>
+        <p>Hi {userState().user?.full_name || userState().user?.name},</p>
+      </Show>
+      <p>
+        {
+          `Thank you for your generosity! You've just made a significant impact on ${props.project.creator.name
+          }'s work. This is just the beginning of our journey together towards change. Did you know that most of our supporters feel an even deeper connection to the cause when they share their commitment with others? By using this affiliate link to invite your friends and family to join our mission, you're not only amplifying your impact but also becoming part of a larger community dedicated to making a difference. Let's create a wave of positive change together. Share your support now and watch our collective impact grow!`
+        }
+      </p>
+      <p>
+        {
+          "P.S. You can also share your link on social media and in your email signature. The more people you reach, the more you can earn!"
+        }
+      </p>
+      <img src="https://imagedelivery.net/xHZxKHrwCcaO8iTco-Njhg/14b9a8f7-9183-4768-b53f-f77cd8a87f00/small" alt="Ryan's signature" class="w-24 aspect-video mx-4" />
+
+    </div>
+  )
+};
+
+export default ThankYouLetter;
