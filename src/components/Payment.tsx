bbb03daf76d6a3b307564d6136317ecfe27a6896
Ryan Trainor <rptrainor@gmail.com>
2023-12-21 11:27:52 -0300

    correctly set values of payment form

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -53,8 +53,8 @@ type PaymentProps = {
 export default function Payment(props: PaymentProps) {
   const [stripe, setStripe] = createSignal<Stripe | null>(null)
   const [clientSecret, setClientSecret] = createSignal<string | undefined>(undefined)
-  const [amount, setAmount] = createSignal<number>(0)
-  const [tipPercent, setTimePercent] = createSignal<number>(0);
+  const [amount, setAmount] = createSignal<number>(47)
+  const [tipPercent, setTimePercent] = createSignal<number>(0.2);
   const [isCustomTip, setIsCustomTip] = createSignal(false);
   // IBAN: Test account number: DE89370400440532013000
   const [currency, setCurrency] = createSignal('usd');
@@ -62,8 +62,7 @@ export default function Payment(props: PaymentProps) {
   const [customerId, setCustomerId] = createSignal<string | undefined>(undefined);
   const [customerEmail, setCustomerEmail] = createSignal<string | undefined>(undefined);
   const [customerName, setCustomerName] = createSignal<string | undefined>(undefined);
-  const [totalAmount, setTotalAmount] = createSignal<number>(0);
-
+  const [totalAmount, setTotalAmount] = createSignal<number>(56);
 
   onMount(async () => {
     const stripeResult = await loadStripe(import.meta.env.PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST)
@@ -85,7 +84,7 @@ export default function Payment(props: PaymentProps) {
       });
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: &dollar;{response.status}`);
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const data = await response.json();
@@ -116,7 +115,7 @@ export default function Payment(props: PaymentProps) {
       });
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: &dollar;{response.status}`);
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const data = await response.json();
@@ -214,7 +213,7 @@ export default function Payment(props: PaymentProps) {
       });
 
       if (!response.ok) {
-        throw new Error(`HTTP error! status: &dollar;{response.status}`);
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
 
       const data = await response.json();
@@ -227,6 +226,23 @@ export default function Payment(props: PaymentProps) {
       console.error("Error creating subscription:", error);
       return undefined;
     }
+  }
+
+  // Handler for donation amount change
+  const handleDonationAmountChange = (amount: number) => {
+    setAmount(amount);
+  };
+
+  // Handler for recurring payment option
+  const handleRecurringChange = (event: Event) => {
+    const value = (event.target as HTMLInputElement).value;
+    setIsRecurring(value === 'yes');
+  };
+
+  const handleCurrencyChange = (value: string) => {
+    const newAmount = value === 'usd' ? 47 : 43
+    setCurrency(value);
+    setAmount(newAmount)
   }
 
   createEffect(() => {
@@ -244,31 +260,33 @@ export default function Payment(props: PaymentProps) {
       tipPercent: tipPercent(),
       isCustomTip: isCustomTip(),
       isRecurring: isRecurring(),
-      totalAmount: totalAmount()
+      totalAmount: totalAmount(),
+      clientSecret: clientSecret()
     });
   })
 
   return (
     <Switch fallback={null}>
       <Match when={!clientSecret()}>
-        <form class='flex flex-col gap-4 w-full p-4 mx-auto max-w-sm'>
-
-          <Show when={currency() === 'eur'} fallback={<USDButtons />}>
-            <EURButtons />
+        <div class='flex flex-col gap-4 w-full p-4 mx-auto max-w-sm'>
+          {/* Donation Amount Buttons */}
+          <Show when={currency() === 'eur'} fallback={<USDButtons onAmountChange={handleDonationAmountChange} />}>
+            <EURButtons onAmountChange={handleDonationAmountChange} />
           </Show>
 
+          {/* Recurring Payment Option */}
           <div class="grid w-full items-center gap-1.5">
             <label class="">
               Make it monthly!
             </label>
             <fieldset class="grid grid-cols-2 gap-2">
               <label class="relative flex items-center justify-center">
-                <input type="radio" id="yes" name="sustaining membership" role="radio" class="peer sr-only" value="yes" />
+                <input type="radio" onChange={handleRecurringChange} id="yes" name="sustaining membership" role="radio" class="peer sr-only" value="yes" />
                 <span class="absolute z-10 text-brand_black">Yes, Let's go!</span>
                 <div class="w-full h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
               </label>
               <label class="relative flex items-center justify-center">
-                <input checked type="radio" id="no" name="sustaining membership" role="radio" class="peer sr-only" value="no" />
+                <input checked type="radio" onChange={handleRecurringChange} id="no" name="sustaining membership" role="radio" class="peer sr-only" value={"no"} />
                 <span class="absolute z-10 text-brand_black">No, give once</span>
                 <div class="w-full h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
               </label>
@@ -276,9 +294,7 @@ export default function Payment(props: PaymentProps) {
           </div>
 
           <div class="grid w-full items-center gap-1.5">
-            <label class="">
-              Add a tip to support Lightup
-            </label>
+            {/* Tip Selection */}
             <select onChange={handleTipChange} class="w-full border-4 border-brand_black bg-brand_white text-brand_black p-2 hover:border-brand_pink focus:border-brand_pink focus:ring focus:ring-brand_pink focus:ring-opacity-50 transition ease-in-out duration-150">
               <option value={0}>0%</option>
               <option value={0.05}>5%</option>
@@ -304,24 +320,28 @@ export default function Payment(props: PaymentProps) {
             </select>
           </div>
           <div>
-            <input type='submit' onClick={handlePaymentIntent} class='bg-brand_pink text-brand_black sm:px-6 border-4 border-brand_black to-brand_black w-full sm:mt-2 uppercase gap-2 group z-20 text-3xl' value='Donate' />
+            {/* Submit Button */}
+            <button onClick={handlePaymentIntent} class='bg-brand_pink text-brand_black sm:px-6 border-4 border-brand_black to-brand_black w-full sm:mt-2 uppercase gap-2 group z-20 text-3xl' >
+              <span class='group-hover:text-brand_pink'>Donate</span>
+            </button>
+            {/* Currency Switch */}
             <fieldset class="grid grid-cols-4 gap-2">
               <Show when={currency() === 'eur'} fallback={<legend class='pb-2'>Switch to <span>&euro;</span>:</legend>}>
                 <legend class='pb-2'>Switch to <span>&dollar;</span>:</legend>
               </Show>
               <div class="flex items-center h-4 gap-6 space-y-0">
                 <div class="flex items-center gap-0">
-                  <input id="usd" value='usd' name="notification-method" type="radio" checked={currency() === 'usd'} onChange={() => setCurrency('usd')} class="h-4 w-4 border-brand_black text-brand_pink focus:ring-brand_pink" />
+                  <input id="usd" value='usd' name="notification-method" type="radio" checked={currency() === 'usd'} onChange={() => handleCurrencyChange('usd')} class="h-4 w-4 border-brand_black text-brand_pink focus:ring-brand_pink" />
                   <label for="usd" class="ml-2 block text-sm font-medium leading-6 text-brand_white">&dollar;</label>
                 </div>
                 <div class="flex items-center gap-0">
-                  <input id="eur" value='eur' name="notification-method" type="radio" checked={currency() === 'eur'} onChange={() => setCurrency('eur')} class="h-4 w-4 border-brand_black text-brand_pink focus:ring-brand_pink" />
+                  <input id="eur" value='eur' name="notification-method" type="radio" checked={currency() === 'eur'} onChange={() => handleCurrencyChange('eur')} class="h-4 w-4 border-brand_black text-brand_pink focus:ring-brand_pink" />
                   <label for="eur" class="ml-2 block text-sm font-medium leading-6 text-brand_white">&euro;</label>
                 </div>
               </div>
             </fieldset>
           </div>
-        </form>
+        </div>
       </Match>
       <Match when={stripe() && clientSecret()}>
         <Elements stripe={stripe()} clientSecret={clientSecret()}>
@@ -333,6 +353,6 @@ export default function Payment(props: PaymentProps) {
           />
         </Elements>
       </Match>
-    </Switch>
+    </Switch >
   )
 }
--- src/components/USDButtons.tsx
+++ src/components/USDButtons.tsx
@@ -1,48 +1,155 @@
-const USDButtons = () => {
+import { createSignal } from "solid-js";
+
+type Props = {
+  onAmountChange: (amount: number) => void;
+}
+
+const USDButtons = (props: Props) => {
+  const [selectedAmount, setSelectedAmount] = createSignal<number | null>(47);
+
+  const handleAmountChange = (amount: number) => {
+    setSelectedAmount(amount);
+    props.onAmountChange(amount);
+  };
+
+  const handleCustomAmountChange = (event: Event) => {
+    const value = (event.target as HTMLInputElement).value;
+    const numValue = value ? parseInt(value, 10) : null;
+
+    setSelectedAmount(numValue);
+
+    if (numValue !== null) {
+      props.onAmountChange(numValue);
+    }
+  };
+
+  const isCustomSelected = () => {
+    const amount = selectedAmount();
+    return amount === null || ![47, 72, 106, 39, 23, 17, 6].includes(amount);
+  };
+
+
   return (
     <fieldset class="grid grid-cols-4 gap-2">
       <legend class='pb-2'>Choose an amount:</legend>
       <label class="relative flex items-center justify-center">
-        <input checked aria-checked="true" type="radio" id="50" name="donation_amount" role="radio" class="peer sr-only" value="50" />
+        <input
+          checked
+          aria-checked="true"
+          type="radio"
+          id="47"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="47"
+          onChange={() => handleAmountChange(47)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;47</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
       <label class="relative flex items-center justify-center">
-        <input type="radio" id="75" name="donation_amount" role="radio" class="peer sr-only" value="75" />
+        <input
+          type="radio"
+          id="72"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="72"
+          onChange={() => handleAmountChange(72)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;72</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
 
       <label class="relative flex items-center justify-center">
-        <input type="radio" id="100" name="donation_amount" role="radio" class="peer sr-only" value="100" />
+        <input
+          type="radio"
+          id="106"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="106"
+          onChange={() => handleAmountChange(106)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;106</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
       <label class="relative flex items-center justify-center">
-        <input type="radio" id="40" name="donation_amount" role="radio" class="peer sr-only" value="40" />
+        <input
+          type="radio"
+          id="39"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="39"
+          onChange={() => handleAmountChange(39)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;39</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
       <label class="relative flex items-center justify-center">
-        <input type="radio" id="25" name="donation_amount" role="radio" class="peer sr-only" value="25" />
+        <input
+          type="radio"
+          id="23"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="23"
+          onChange={() => handleAmountChange(23)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;23</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
       <label class="relative flex items-center justify-center">
-        <input type="radio" id="15" name="donation_amount" role="radio" class="peer sr-only" value="15" />
+        <input
+          type="radio"
+          id="17"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="17"
+          onChange={() => handleAmountChange(17)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;17</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
       <label class="relative flex items-center justify-center">
-        <input type="radio" id="10" name="donation_amount" role="radio" class="peer sr-only" value="10" />
+        <input
+          type="radio"
+          id="6"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="6"
+          onChange={() => handleAmountChange(6)}
+        />
         <span class="absolute z-10 text-brand_black">&dollar;6</span>
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-focus:ring-2 peer-focus:ring-brand_pink peer-focus:ring-offset-2 peer-focus:ring-offset-brand_white peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors"></div>
       </label>
       {/* Custom amount input */}
       <label class="relative flex flex-col items-center justify-center text-brand_black">
-        <input type="radio" id="custom_amount_radio" name="donation_amount" role="radio" class="peer sr-only" value="custom" />
+        <input
+          type="radio"
+          id="custom_amount_radio"
+          name="donation_amount"
+          role="radio"
+          class="peer sr-only"
+          value="custom"
+          checked={isCustomSelected()}
+          onChange={handleCustomAmountChange}
+        />
         <div class="w-[4.5rem] h-12 bg-brand_white peer-checked:bg-brand_pink peer-checked:border-solid peer-checked:border-4 border-brand_black flex items-center justify-center transition-colors">
-          <input type="number" id="custom_amount" name="custom_amount" placeholder="$" class="w-full h-full text-center border-0 peer-checked:bg-brand_pink peer-checked:border-solid peer-checked:border-4 border-brand_black p-2 peer-focus:border-brand_pink peer-focus:ring-2 peer-focus:ring-offset-2 peer-focus:ring-brand_pink transition-colors disabled:bg-gray-100 disabled:text-gray-500" />
+          <input
+            type="number"
+            min={0}
+            incremental
+            id="custom_amount"
+            name="custom_amount"
+            placeholder="$"
+            checked={isCustomSelected()}
+            onInput={handleCustomAmountChange}
+            class={`w-full h-full text-center border-0 peer-checked:bg-brand_pink peer-checked:border-solid peer-checked:border-4 border-brand_black p-2 peer-focus:border-brand_pink peer-focus:ring-2 peer-focus:ring-offset-2 peer-focus:ring-brand_pink transition-colors disabled:bg-gray-100 disabled:text-gray-500 ${isCustomSelected() ? 'bg-brand_pink focus:ring-2 focus:ring-brand_pink focus:ring-offset-2 focus:ring-offset-brand_white border-solid border-1' : 'bg-brand_white'}`}
+          />
         </div>
       </label>
     </fieldset>
