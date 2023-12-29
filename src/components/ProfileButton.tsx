b649438349e257b3e81b272a8fe3cac7df06fbe7
Ryan Trainor <rptrainor@gmail.com>
2023-12-29 11:22:17 -0300

    remove unused a tag

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -2,11 +2,11 @@
 const ProfileButton = () => {
   return (
     <div class="flex justify-center items-center z-10">
-      <a class="w-8 h-8 sm:w-12 sm:h-12 rounded-full bg-brand_black text-brand_pink flex items-center justify-center overflow-hidden" data-astro-prefetch>
+      <div class="w-8 h-8 sm:w-12 sm:h-12 rounded-full bg-brand_black text-brand_pink flex items-center justify-center overflow-hidden" data-astro-prefetch>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 sm:w-12 sm:h-12">
           <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
         </svg>
-      </a>
+      </div>
     </div>
   );
 }
